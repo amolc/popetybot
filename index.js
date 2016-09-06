bd6576c3ad91b99a3d20497f7e43487701a5421f
amolc <amol.chawathe@fountaintechies.com>
2016-09-06 17:32:01 +0800

    hello

--- index.js
+++ index.js
@@ -42,4 +42,7 @@ app.post('/', (req, res) => {
   res.end(JSON.stringify({status: 'ok'}))
 })
 
-http.createServer(app).listen(3000)
+// Spin up the server
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'))
+})
