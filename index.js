64be6a43cba3583ea86768569bebe66a4e6c7d4b
amolc <amol.chawathe@fountaintechies.com>
2016-09-07 11:46:01 +0800

    test and exception

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.post('/webhook/', function (req, res) {
       let sender = event.sender.id
       if (event.message && event.message.text) {
         let text = event.message.text
-        if (text === 'Rent') {
+        if (text === 'Rent' || text === 'rent') {
             propertyDetail(sender)
             continue
         }
